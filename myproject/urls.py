e2833479e25fb96e39e7ab3c2ba48b655462fd50
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-02-12 15:22:13 +0200

    2/12/19

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from boards import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('boards/<pk>/', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]
